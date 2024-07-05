5fc482aeb6cb0adf0799729525794ea42d2d0ba3
bishaludash <supertramp.bishal@gmail.com>
2024-07-05 12:00:27 +0200

    add some renposiveness

--- src/App.jsx
+++ src/App.jsx
@@ -56,14 +56,14 @@ const App = () => {
   };
 
   return (
-    <div className=" bg-gradient-to-r from-sky-500 to-indigo-500 text-center items-center h-screen p-40">
+    <div className=" bg-gradient-to-r from-sky-500 to-indigo-500 text-center items-center h-screen xl:p-40 sm:p-10">
       {/* App Title */}
-      <h1 className=" text-5xl font-bold font-mono my-5 ">
+      <h1 className=" text-5xl font-bold font-mono py-5 ">
         Budash Weather App
       </h1>
 
       {/* container */}
-      <div className="w-2/4 m-auto">
+      <div className="xl:w-2/4 sm:p-5 m-auto">
         <SearchBar
           cityName={cityName}
           setCityName={setCityName}
